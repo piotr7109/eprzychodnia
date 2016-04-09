857945956232c4923de5c9eb94dfef5c9d3415f4
piotr <piotr@piotr-VirtualBox>
2016-04-09 21:31:22 +0200

    zmiana założeń dotyczących bazy

--- eprzychodnia/public_html/app/components/home/home.ts
+++ eprzychodnia/public_html/app/components/home/home.ts
@@ -8,46 +8,35 @@ import {Uzytkownik}  from '/app/modules/uzytkownik/uzytkownik.ts';
 import {Http}  from 'angular2/http';
 //import {BaseComponent} from 'app/components/base_component.ts';
 @Component({
-  selector: 'home',
-  templateUrl:"/app/views/home/home.html" 
+    selector: 'home',
+    templateUrl: "/app/views/home/home.html"
 })
 
 @CanActivate(() => czyZalogowany())
 export class Home implements OnInit {
-  public uzytkownik:Uzytkownik = new Uzytkownik();
-  public id_uzytkownika:number = 110;
-  constructor(public auth: Authentication, public router: Router, public http:Http) 
-  {
-      this.uzytkownik.setLogin("DEFAULT");
-      //this.ngOnInit();
+    public uzytkownik: Uzytkownik = new Uzytkownik();
+    public id_uzytkownika: number;
+    constructor(public auth: Authentication, public router: Router, public http: Http) {
 
-      
-  }
+        this.uzytkownik.setLogin("DEFAULT");
+        this.id_uzytkownika = localStorage.getItem('token');
 
-  ngOnInit()
-  {
-      alert("init");
-      var id_uzytkownika: number = localStorage.getItem('token');
-      this.http.get('http://localhost:5984/eprzychodnia/_design/users/_view/getUser?key=["lekarz","arr2"]')
-        .subscribe((data) => {
-            this.id_uzytkownika = 10;
-      });
-      
-      /*this.uzytkownik = UzytkownikFactory.getUzytkownik(id_uzytkownika)
-      .then(
-          (uzytkownik:Uzytkownik) => {this.uzytkownik = uzytkownik}
-          
-      )
-      .then
-      (
-        () => { this.router.navigate(['/Home']); }
-      );*/
-      
-  }
-  onLogout() {
-    this.auth.logout()
-      .subscribe(
-        () => this.router.navigate(['../Login']),
-      );
-  }
+        UzytkownikFactory.getUzytkownik(http, this.id_uzytkownika)
+            .subscribe((uzytkownik: Uzytkownik) => {
+                this.uzytkownik = uzytkownik;
+            });
+
+
+    }
+
+    ngOnInit() {
+        alert("init");
+
+    }
+    onLogout() {
+        this.auth.logout()
+            .subscribe(
+            () => this.router.navigate(['../Login']),
+        );
+    }
 }
