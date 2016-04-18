d6758145a250e44f2c26ad0d1d64e402d862cd1c
dominik59 <dominik-pawlik1@wp.pl>
2016-04-18 17:30:50 +0200

    Zmiana widoku polegająca na zmianie właściwości opacity kolumny bocznej.

--- eprzychodnia/public_html/assets/scripts/logowanie.js
+++ eprzychodnia/public_html/assets/scripts/logowanie.js
@@ -6,38 +6,45 @@
 if($(document).ready())
 {
     $(".home_page_well").css({opacity:0.5});
-    $(window).scroll(function (event){
-        
-        var scrolling = $(window).scrollTop();
-        var opacity = $(".home_page_jumbotron").css("opacity");
-        
-        //var opacity = $(".jumbotron").opacity();
-        //alert(opacity);
-        
-        $(".home_page_well").css({opacity:0.5+scrolling/500});
-        //$(".home_page_jumbotron").css({opacity:1-scrolling/3000});
-        
-    });
+//    $(window).scroll(function (event){
+//        
+//        var scrolling = $(window).scrollTop();
+//        var opacity = $(".home_page_jumbotron").css("opacity");
+//        
+//        //var opacity = $(".jumbotron").opacity();
+//        //alert(opacity);
+//        
+//        $(".home_page_well").css({opacity:0.5+scrolling/500});
+//        //$(".home_page_jumbotron").css({opacity:1-scrolling/3000});
+//        
+//    });
     $("#system_epop_h6").hide();
     $("#kto_moze_korzystac_h6").hide();
     $("#co_zrobic_uzytkownik_h6").hide();
+    
     $(".system_epop").hover(function ()
     {
         showElement($("#system_epop_h6"),100);
+        $(".system_epop").css({opacity:1});
     }, function(){
         hideElement($("#system_epop_h6"),100);
+        $(".system_epop").css({opacity:0.5});
     });
     $(".co_zrobic_uzytkownik").hover(function ()
     {
         showElement($("#co_zrobic_uzytkownik_h6"),100);
+        $(".co_zrobic_uzytkownik").css({opacity:1});
     }, function(){
         hideElement($("#co_zrobic_uzytkownik_h6"),100);
+        $(".co_zrobic_uzytkownik").css({opacity:0.5});
     });
     $(".kto_moze_korzystac").hover(function ()
     {
         showElement($("#kto_moze_korzystac_h6"),100);
+        $(".kto_moze_korzystac").css({opacity:1});
     }, function(){
         hideElement($("#kto_moze_korzystac_h6"),100);
+        $(".kto_moze_korzystac").css({opacity:0.5});
     });
 }
 function showElement(element,time)
