b90e8a0ba8252c90a3de5a8997026d9134fb140f
michal <michal@michal-N53SV>
2016-06-06 18:53:42 +0200

    test2

--- eprzychodnia/public_html/testy/1st.spec.ts
+++ eprzychodnia/public_html/testy/1st.spec.ts
@@ -1,9 +1,8 @@
-import {ValidationService} from "app/services/form/form_helpers.ts";
-var pesel = 92060603556;
+import {ValidationService} from 'testy/form_helpers.ts'
 describe('1st tests', () => {
   it('true is true', () => expect(true).toEqual(true));
   it('null is not the same thing as undefined',
-  () => expect(true).toEqual(sprawdzPesel(pesel))
+  () => expect(true).toEqual(peselValidator(92060603556))
 );
 });
 
