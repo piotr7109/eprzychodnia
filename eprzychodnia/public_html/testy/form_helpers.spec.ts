1aa48c97699fa4f7ad614189665a7f07a6219309
michal <michal@michal-N53SV>
2016-06-06 21:30:24 +0200

    testPESEL OK

--- eprzychodnia/public_html/testy/form_helpers.spec.ts
+++ eprzychodnia/public_html/testy/form_helpers.spec.ts
@@ -5,7 +5,7 @@ describe('PESEL', () => {
       let pesels = new Array("97021919969", "75090610255", "38063019473", "40111217324", "28041700631", "67052416911", "86042612768", "53051214100", "72070507839", "88122604717");
       for(let ob in pesels)
       {
-        obiekt.value = ob;
+        obiekt.value = pesels[ob];
         expect(null).toEqual(ValidationService.peselValidator(obiekt));
       }
       
@@ -13,10 +13,10 @@ describe('PESEL', () => {
   });
   it('zly pesel', () => {
       let obiekt = [];
-      let pesels = new Array("97021919967", "75090610254", "38063019473", "40111217327", "28041700638", "67052416912", "86042612764", "53021214100", "72d70507839", "88102604717");
+      let pesels = new Array("97021919967", "75090610254", "3806301947t", "40111217327", "28041700638", "67052416912", "86042612764", "53021214100", "72d70507839", "88102604717");
       for(let ob in pesels)
       {
-        obiekt.value = ob;
+        obiekt.value = pesels[ob];
         expect(null).not.toEqual(ValidationService.peselValidator(obiekt));
       }
 
