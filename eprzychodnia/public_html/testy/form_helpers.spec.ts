1ce9560df3e5e1cb64a343821ccb9622aaa9d14f
michal <michal@michal-N53SV>
2016-06-06 21:24:17 +0200

    test5

--- eprzychodnia/public_html/testy/form_helpers.spec.ts
+++ eprzychodnia/public_html/testy/form_helpers.spec.ts
@@ -1,17 +1,24 @@
 import { ValidationService } from '/testy/form_helpers.ts';
 describe('PESEL', () => {
   it('dobry pesel', () => {
-      let obiekt;
-      obiekt.value = 92060603556;
-      expect(null).toEqual(ValidationService.peselValidator(obiekt));
+      let obiekt = [];
+      let pesels = new Array("97021919969", "75090610255", "38063019473", "40111217324", "28041700631", "67052416911", "86042612768", "53051214100", "72070507839", "88122604717");
+      for(let ob in pesels)
+      {
+        obiekt.value = ob;
+        expect(null).toEqual(ValidationService.peselValidator(obiekt));
+      }
       
 
   });
   it('zly pesel', () => {
-      let obiekt;
-      obiekt.value = 92060603557;
-      expect(null).not.toEqual(ValidationService.peselValidator(obiekt));
-      
+      let obiekt = [];
+      let pesels = new Array("97021919967", "75090610254", "38063019473", "40111217327", "28041700638", "67052416912", "86042612764", "53021214100", "72d70507839", "88102604717");
+      for(let ob in pesels)
+      {
+        obiekt.value = ob;
+        expect(null).not.toEqual(ValidationService.peselValidator(obiekt));
+      }
 
   });
 });
