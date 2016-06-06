b3052cb5369755718f7352867c4c70ed31be0f9e
michal <michal@michal-N53SV>
2016-06-06 19:07:40 +0200

    test3a

--- eprzychodnia/public_html/testy/1st.spec.ts
+++ eprzychodnia/public_html/testy/1st.spec.ts
@@ -1,8 +1,21 @@
-describe('1st tests', () => {
-  it('true is true', () => expect(true).toEqual(true));
-  it('null is not the same thing as undefined',
-  () => expect(true).toEqual(true)
-);
-});
+import {ValidationService} from 'app/services/form/form_helpers.ts'
+
+describe('ValidationService'), () => {
+    if('pesel', () => {
+        let obiekt;
+        obiekt.value = 92060603556;
+        let peselOk = peselValidator(obiekt)
+        expect(peselOk).toEqual(true);
+    })
+}
+    
+        
+            
+ //               describe('1st tests', () => {
+  //it('true is true', () => expect(true).toEqual(true));
+  //it('null is not the same thing as undefined',
+ // () => expect(true).toEqual(true)
+//);
+//});
 
 
