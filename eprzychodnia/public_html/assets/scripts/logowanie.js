625b632b73e284cfdd2de135dc5ce0d38876b655
dominik59 <dominik-pawlik1@wp.pl>
2016-06-21 02:23:11 +0200

    zmiana kalendarza

--- eprzychodnia/public_html/assets/scripts/logowanie.js
+++ eprzychodnia/public_html/assets/scripts/logowanie.js
@@ -95,17 +95,139 @@ function init_rejestracja_pacjenta()
 }
 function show_doctors_calendar()
 {
-    $("#kalendarz_lekarza").html("");
-    $("#kalendarz_lekarza").monthly({mode: 'event',
-    // The element that will have its value set to the date you picked
-    target: '#mytarget',
-    // Set to true if you want monthly to appear on click
-    maxWidth:'587px',
-    xmlUrl: 'assets/scripts/monthly.xml',
-    // Add a style to days in the past
-    stylePast: true,
-    // Disable clicking days in the past
-    disablePast: true});
+//    $("#kalendarz_lekarza").html("");
+//    $("#kalendarz_lekarza").monthly({mode: 'event',
+//    // The element that will have its value set to the date you picked
+//    target: '#mytarget',
+//    // Set to true if you want monthly to appear on click
+//    maxWidth:'587px',
+//    xmlUrl: 'assets/scripts/monthly.xml',
+//    // Add a style to days in the past
+//    stylePast: true,
+//    // Disable clicking days in the past
+//    disablePast: true});
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+
+    /*
+            Initialize fullCalendar and store into variable.
+            Why in variable?
+            Because doing so we can use it inside other function.
+            In order to modify its option later.
+    */
+
+    var calendar = $('#kalendarz_lekarza').fullCalendar(
+    {
+            /*
+                    header option will define our calendar header.
+                    left define what will be at left position in calendar
+                    center define what will be at center position in calendar
+                    right define what will be at right position in calendar
+            */
+            header:
+            {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay'
+            },
+            /*
+                    defaultView option used to define which view to show by default,
+                    for example we have used agendaWeek.
+            */
+            defaultView: 'month',
+            /*
+                    selectable:true will enable user to select datetime slot
+                    selectHelper will add helpers for selectable.
+            */
+            selectable: true,
+            selectHelper: true,
+            /*
+                    when user select timeslot this option code will execute.
+                    It has three arguments. Start,end and allDay.
+                    Start means starting time of event.
+                    End means ending time of event.
+                    allDay means if events is for entire day or not.
+            */
+            select: function(start, end, allDay)
+            {
+                    /*
+                            after selection user will be promted for enter title for event.
+                    */
+                    var title = prompt('Event Title:');
+                    /*
+                            if title is enterd calendar will add title and event into fullCalendar.
+                    */
+                    if (title)
+                    {
+                            calendar.fullCalendar('renderEvent',
+                                    {
+                                            title: title,
+                                            start: start,
+                                            end: end,
+                                            allDay: allDay
+                                    },
+                                    true // make the event "stick"
+                            );
+                    }
+                    calendar.fullCalendar('unselect');
+            },
+            /*
+                    editable: true allow user to edit events.
+            */
+            editable: true,
+            /*
+                    events is the main option for calendar.
+                    for demo we have added predefined events in json object.
+            */
+            events: [
+                    {
+                            title: 'All Day Event',
+                            start: new Date(y, m, 1)
+                    },
+                    {
+                            title: 'Long Event',
+                            start: new Date(y, m, d-5),
+                            end: new Date(y, m, d-2)
+                    },
+                    {
+                            id: 999,
+                            title: 'Repeating Event',
+                            start: new Date(y, m, d-3, 16, 0),
+                            allDay: false
+                    },
+                    {
+                            id: 999,
+                            title: 'Repeating Event',
+                            start: new Date(y, m, d+4, 16, 0),
+                            allDay: false
+                    },
+                    {
+                            title: 'Meeting',
+                            start: new Date(y, m, d, 10, 30),
+                            allDay: false
+                    },
+                    {
+                            title: 'Lunch',
+                            start: new Date(y, m, d, 12, 0),
+                            end: new Date(y, m, d, 14, 0),
+                            allDay: false
+                    },
+                    {
+                            title: 'Birthday Party',
+                            start: new Date(y, m, d+1, 19, 0),
+                            end: new Date(y, m, d+1, 22, 30),
+                            allDay: false
+                    },
+                    {
+                            title: 'Click for Google',
+                            start: new Date(y, m, 28),
+                            end: new Date(y, m, 29),
+                            url: 'http://google.com/'
+                    }
+            ]
+    });
 }
 function show_alert()
 {
@@ -151,6 +273,7 @@ function init_home_page()
         element.hide(time);
     }
 }
+
 
 
 
